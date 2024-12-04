cf0f67981e1b2275d6885a4cf33f280ad1bc84eb
VB2007 <admin@vb2007.hu>
2024-12-05 00:33:28 +0100

    modifications

--- src/assets/js/navbar.js
+++ src/assets/js/navbar.js
@@ -1,17 +1,8 @@
 const navToggler = document.querySelector('.nav--toggler');
 const navCollapse = document.querySelector('.nav--collapse');
-let isOpen = false;
+const navWrapper = document.querySelector('.nav--wrapper');
 
-navToggler.addEventListener('click', function() {
-    const contentHeight = navCollapse.scrollHeight;
-    
-    if (!isOpen) {
-        navCollapse.style.height = contentHeight + 'px';
-        navCollapse.classList.add('show');
-    } else {
-        navCollapse.style.height = '0px';
-        navCollapse.classList.remove('show');
-    }
-    
-    isOpen = !isOpen;
+navToggler.addEventListener('click', () => {
+    navCollapse.classList.toggle('show');
+    navWrapper.classList.toggle('show');
 });
