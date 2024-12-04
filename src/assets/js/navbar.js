2b56762b2ee928ac3777b188f309b7b66a672d3b
VB2007 <admin@vb2007.hu>
2024-12-04 23:11:44 +0100

    added smooth opening animation to responsive navbar

--- src/assets/js/navbar.js
+++ src/assets/js/navbar.js
@@ -1,3 +1,17 @@
-document.querySelector('.nav--toggler').addEventListener('click', function() {
-    document.querySelector('.nav--collapse').classList.toggle('show');
+const navToggler = document.querySelector('.nav--toggler');
+const navCollapse = document.querySelector('.nav--collapse');
+let isOpen = false;
+
+navToggler.addEventListener('click', function() {
+    const contentHeight = navCollapse.scrollHeight;
+    
+    if (!isOpen) {
+        navCollapse.style.height = contentHeight + 'px';
+        navCollapse.classList.add('show');
+    } else {
+        navCollapse.style.height = '0px';
+        navCollapse.classList.remove('show');
+    }
+    
+    isOpen = !isOpen;
 });
