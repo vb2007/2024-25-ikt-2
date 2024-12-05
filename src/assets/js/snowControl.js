5738151f6e742f3672b98d3d846e044f30f84795
VB2007 <admin@vb2007.hu>
2024-12-05 02:06:40 +0100

    changed design & javascript for disabling snowfall

--- src/assets/js/snowControl.js
+++ src/assets/js/snowControl.js
@@ -1,20 +1,22 @@
 let isSnowing = true;
-const snowButton = document.querySelector('.nav--button');
-const snowflakes = document.querySelector('.snowflakes');
+const snowButton = document.querySelector(".nav--button");
+const snowflakes = document.querySelector(".snowflakes");
 
 function toggleSnow() {
     if (isSnowing) {
-        snowflakes.style.display = 'none';
-        snowButton.textContent = 'Hóesés BE';
+        snowflakes.style.display = "none";
+        snowButton.classList.remove("nav--button");
+        snowButton.classList.add("nav--button--disabled");
     }
     else {
-        snowflakes.style.display = 'block';
-        snowButton.textContent = 'Hóesés KI';
+        snowflakes.style.display = "block";
+        snowButton.classList.remove("nav--button--disabled");
+        snowButton.classList.add("nav--button");
     }
     isSnowing = !isSnowing;
 }
 
-snowButton.addEventListener('click', function(e) {
+snowButton.addEventListener("click", function(e) {
     e.preventDefault();
     toggleSnow();
 });
